7d358073ff9fc7c5965dd90042b2bbc40568f8fd
Loomloo <loomloo.m@mail.ru>
2017-03-14 01:21:49 +0300

    Add files via upload

---
+++ ems-db/proba1.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE 		Region
+(
+	id 			bigint		PRIMARY KEY,
+	region 			varchar(150)	NOT NULL,
+	City			varchar(150)	NOT NULL
+);
+
+CREATE TABLE 		Area
+(
+	area	 		varchar(150)	NOT NULL,
+	dtype  integer CONSTRAINT  positive_dtype CHECK (dtype = 1)
+)INHERITS (Region);
+
+CREATE TABLE 		District
+(
+	district	 	varchar(150)	NOT NULL,
+	dtype  integer CONSTRAINT  positive_dtype CHECK (dtype = 2)
+)INHERITS (Region);
